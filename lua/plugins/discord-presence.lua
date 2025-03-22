d14301f7eb1be7c7a698b2bf1f33730f4ecdea58
Felipogdev <felipedantaslaurentino@gmail.com>
2025-03-22 20:00:36 +0000

    Feat: Discord Rich-Presence Plugin

---
+++ lua/plugins/discord-presence.lua
@@ -0,0 +1,21 @@
+return {
+  {
+    "andweeb/presence.nvim",
+    lazy = false, -- Load immediately
+    config = function()
+      require("presence").setup({
+        auto_update = true,
+        neovim_image_text = "LagoNvim",
+        main_image = "neovim",
+        client_id = "793271441293967371",
+        log_level = "error",
+        debounce_timeout = 10,
+        enable_line_number = false,
+        show_time = true,
+        editing_text = "Editing %s",
+        file_explorer_text = "Browsing %s",
+        git_commit_text = "Committing changes...",
+      })
+    end,
+  },
+}
