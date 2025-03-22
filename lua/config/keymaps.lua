b84953868792b9744b541ced5bf0b8897b27614c
daniel <skyrimdaniel11@gmail.com>
2025-03-22 01:35:23 -0300

    feat: add keymaps and add new colorschemes

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,18 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+--declare default opts
+local opts = { noremap = true, silent = true }
+
+----> START BUFFER LINE KEYMAPS <----
+
+vim.keymap.set("n", "<Tab>", "<Cmd>BufferLineCycleNext<CR>", opts)
+vim.keymap.set("n", "<S-Tab>", "<Cmd>BufferLineCyclePrev<CR>", opts)
+
+----> END BUFFER LINE KEYMAPS <----
+
+----> START TERMINAL KEYMAPS <----
+
+vim.keymap.set("n", "<C-j>", "<Cmd>terminal<CR>", opts)
+vim.keymap.set("n", "<F7>", "<Cmd>terminal<CR>", opts)
---
+++ lua/plugins/colorscheme.lua
@@ -0,0 +1,30 @@
+return {
+
+  --catppuccin
+  {
+    "catppuccin/nvim",
+    name = "catppuccin",
+    lazy = false,
+  },
+
+  --visual studio code
+  {
+    "Mofiqul/vscode.nvim",
+    name = "vscode",
+    lazy = false,
+  },
+
+  --oh-lucy
+  {
+    "Yazeed1s/oh-lucy.nvim",
+    name = "oh-lucy",
+    lazy = false,
+  },
+
+  --dracula
+  {
+    "Mofiqul/dracula.nvim",
+    name = "dracula",
+    lazy = false,
+  },
+}
