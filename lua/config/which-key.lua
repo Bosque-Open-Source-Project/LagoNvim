b7fb4b31ed34d49603ebabf799dc2688589721bc
Felipogdev <felipedantaslaurentino@gmail.com>
2025-03-24 20:21:11 +0000

    Revert "Merge pull request #9 from Bosque-Open-Source-Project/LN-11"
    This reverts commit 7829c1c51c5432d971c1c0e58dba041e46b8e2cf, reversing
    changes made to 1b9f970cb445392d50e3c63ca029fb5fcf0577eb.

--- lua/config/which-key.lua
+++
@@ -1,9 +0,0 @@
-local wk = require("which-key")
-
-wk.add({
-  { "<leader>ct", group = "tools" },
-  { "<leader>ctl", group = "live server" },
-  { "<leader>cL", group = "languages" },
-  { "<leader>cLt", group = "ts" },
-  { "<leader>cLc", group = "c/c++" },
-})
